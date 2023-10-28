af8f4ac7df53da3cc65f92979f66fcafe7c38e44
AndariYasintaPermana <2110631170147@student.unsika.ac.id>
2023-10-28 19:04:05 +0700

    challenge1

---
+++ resources/views/content.blade.php
@@ -0,0 +1,11 @@
+@extends ('template')
+
+@section('biodata')
+<div class="about" align= "center">
+    <h3>Tentang Saya</h3>
+    <div class="biodata">
+        <p>Saya adalah Andari Yasinta Permana, seorang mahasiswa semester 5 di Universitas Singgaperbangsa Karawang. Saat ini, saya tengah menekuni jurusan Ilmu Komputer dengan Program Studi Informatika. Saya tinggal di Karawang, di mana saya menjalani perjalanan pendidikan dan menjalin pengalaman berharga dalam dunia akademik. </p>
+    </div>
+</div>
+<br/>
+@endsection
